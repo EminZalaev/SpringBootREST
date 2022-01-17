07bdc266ac9cba5dfc60bbf9342833ca38694a00
EminZalaev <95588733+EminZalaev@users.noreply.github.com>
2022-01-17 21:32:35 +0300

    Update EmployeeDAOImpl.java

--- src/main/java/com/example/project/dao/EmployeeDAOImpl.java
+++ src/main/java/com/example/project/dao/EmployeeDAOImpl.java
@@ -1,12 +1,11 @@
 package com.example.project.dao;
 
 import com.example.project.entity.Employee;
-import org.hibernate.Session;
-import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import java.util.List;
 
 @Repository
@@ -17,30 +16,24 @@ public class EmployeeDAOImpl implements EmployeeDAO {
 
     @Override
     public List<Employee> getAllEmployees() {
-        Session session = entityManager.unwrap(Session.class);
 
-        Query<Employee> query = session.createQuery("from Employee"
-        , Employee.class);
+        Query query = entityManager.createQuery("from Employee");
 
         List<Employee> allEmployees = query.getResultList();
-
-        System.out.println(allEmployees);
 
         return allEmployees;
     }
 
     @Override
     public void saveEmployee(Employee employee) {
-        Session session = entityManager.unwrap(Session.class);
 
-        session.saveOrUpdate(employee);
+        entityManager.merge(employee);
     }
 
     @Override
     public Employee getEmployee(int id) {
-        Session session = entityManager.unwrap(Session.class);
 
-        Employee employee = session.get(Employee.class, id);
+        Employee employee = entityManager.find(Employee.class, id);
         return employee;
     }
 
